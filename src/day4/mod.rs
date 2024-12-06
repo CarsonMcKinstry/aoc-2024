1316f8d9957c836fd1f998caa4cc82af1cad6cb6
Carson McKinstry <carson.mckinstry@gmail.com>
2024-12-06 09:55:15 +0100

    procrastinating day5

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -2,6 +2,8 @@ const PUZZLE_INPUT: &str = include_str!("./puzzle_input.txt");
 
 type Direction = (i32, i32);
 
+struct Matrix(Vec<Vec<String>>);
+
 const DIRECTIONS: [Direction; 8] = [
     // (x, y)
     // nw
@@ -28,24 +30,86 @@ pub(crate) fn run() {
     println!("Part 2: {:?}", part_two(PUZZLE_INPUT));
 }
 
-fn part_one(input: &str) -> u32 {
-    let matrix = input
-        .lines()
-        .map(|line| line.chars().map(|c| c.to_string()).collect::<Vec<String>>())
-        .collect::<Vec<Vec<String>>>();
+impl Matrix {
+    fn n_columns(&self) -> usize {
+        self.0.first().map(|f| f.len()).unwrap()
+    }
 
-    let line_length = matrix.first().map(|f| f.len()).unwrap();
-    let column_length = matrix.len();
+    fn n_rows(&self) -> usize {
+        self.0.len()
+    }
+
+    fn get(&self, x: usize, y: usize) -> String {
+        self.0
+            .get(y)
+            .and_then(|line| line.get(x))
+            .cloned()
+            .unwrap_or_default()
+    }
+
+    fn pairs(&self) -> MatrixCoordinates {
+        MatrixCoordinates {
+            matrix: self,
+            row: 0,
+            col: 0,
+        }
+    }
+}
+
+impl From<Vec<Vec<String>>> for Matrix {
+    fn from(value: Vec<Vec<String>>) -> Self {
+        Self(value)
+    }
+}
+
+impl From<&str> for Matrix {
+    fn from(value: &str) -> Self {
+        Self(
+            value
+                .lines()
+                .map(|line| line.chars().map(|c| c.to_string()).collect::<Vec<String>>())
+                .collect::<Vec<Vec<String>>>(),
+        )
+    }
+}
+
+struct MatrixCoordinates<'a> {
+    matrix: &'a Matrix,
+    row: usize,
+    col: usize,
+}
+
+impl<'a> Iterator for MatrixCoordinates<'a> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.row >= self.matrix.n_rows() {
+            return None;
+        }
+
+        let result = (self.row, self.col);
+
+        self.col += 1;
+
+        if self.col >= self.matrix.n_columns() {
+            self.col = 0;
+            self.row += 1;
+        }
+
+        Some(result)
+    }
+}
+
+fn part_one(input: &str) -> u32 {
+    let matrix = Matrix::from(input);
 
     let mut total: u32 = 0;
 
-    for y in 0..column_length {
-        for x in 0..line_length {
-            for direction in DIRECTIONS {
-                let string = check_direction(&matrix, x, y, &direction, 3);
-                if string == "XMAS" {
-                    total += 1
-                }
+    for (x, y) in matrix.pairs() {
+        for direction in DIRECTIONS {
+            let string = check_direction(&matrix, x, y, &direction, 3);
+            if string == "XMAS" {
+                total += 1
             }
         }
     }
@@ -53,16 +117,8 @@ fn part_one(input: &str) -> u32 {
     total
 }
 
-fn get_matrix_char(matrix: &Vec<Vec<String>>, x: usize, y: usize) -> String {
-    matrix
-        .get(y)
-        .and_then(|line| line.get(x))
-        .cloned()
-        .unwrap_or_default()
-}
-
 fn check_direction(
-    matrix: &Vec<Vec<String>>,
+    matrix: &Matrix,
     x: usize,
     y: usize,
     direction: &Direction,
@@ -72,35 +128,27 @@ fn check_direction(
         .map(|step| {
             let x = (x as i32 + step * direction.0) as usize;
             let y = (y as i32 + step * direction.1) as usize;
-            get_matrix_char(matrix, x, y)
+            matrix.get(x, y)
         })
         .collect()
 }
 
 fn part_two(input: &str) -> u32 {
-    let matrix = input
-        .lines()
-        .map(|line| line.chars().map(|c| c.to_string()).collect::<Vec<String>>())
-        .collect::<Vec<Vec<String>>>();
-
-    let line_length = matrix.first().map(|f| f.len()).unwrap();
-    let column_length = matrix.len();
+    let matrix = Matrix::from(input);
 
     let mut total: u32 = 0;
 
-    for y in 0..column_length - 2 {
-        for x in 0..line_length - 2 {
-            let forward_slash = check_direction(&matrix, x, y, &(1, 1), 2);
-            let back_slash = check_direction(&matrix, x + 2, y, &(-1, 1), 2);
+    for (x, y) in matrix.pairs() {
+        let forward_slash = check_direction(&matrix, x, y, &(1, 1), 2);
+        let back_slash = check_direction(&matrix, x + 2, y, &(-1, 1), 2);
 
-            let mas = String::from("MAS");
-            let sam = String::from("SAM");
+        let mas = String::from("MAS");
+        let sam = String::from("SAM");
 
-            if (forward_slash == mas || forward_slash == sam)
-                && (back_slash == mas || back_slash == sam)
-            {
-                total += 1
-            }
+        if (forward_slash == mas || forward_slash == sam)
+            && (back_slash == mas || back_slash == sam)
+        {
+            total += 1
         }
     }
 
